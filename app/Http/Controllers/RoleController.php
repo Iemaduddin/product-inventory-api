18010c8d4478bb691c07e9d896cb8a208c213996
Iemaduddin <93432453+Iemaduddin@users.noreply.github.com>
2025-07-09 10:41:44 +0700

    add: menambahkan crud role dan middleware checkrole untuk hak akses fitur

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,62 +4,78 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return Role::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try {
+
+            $data = $request->validate(
+                [
+                    'nama_role' => 'required|string|unique:roles,nama_role',
+                    'deskripsi' => 'nullable|string',
+                    'is_active' => 'boolean'
+                ],
+                [
+                    'nama_role.required' => 'Nama role harus diisi.',
+                    'nama_role.unique' => 'Nama role sudah digunakan.',
+                    'is_active.boolean' => 'Status aktif harus berupa boolean.'
+                ]
+            );
+
+            $role = Role::create($data);
+            return response()->json([
+                'message' => 'Role berhasil ditambahkan.',
+                'data' => $role
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validasi gagal.',
+                'errors' => $e->errors(),
+            ], 422);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Role $role)
+    public function show($id)
     {
-        //
+        return Role::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Role $role)
+    public function update(Request $request, $id)
     {
-        //
+        $role = Role::findOrFail($id);
+        try {
+
+            $data = $request->validate([
+                'nama_role' => 'required|string|unique:roles,nama_role,' . $id,
+                'deskripsi' => 'nullable|string',
+                'is_active' => 'boolean'
+            ]);
+
+            $role->update($data);
+            return response()->json([
+                'message' => 'Role berhasil diperbarui.',
+                'data' => $role
+            ], 200);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validasi gagal.',
+                'errors' => $e->errors(),
+            ], 422);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Role $role)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Role $role)
-    {
-        //
+        $role = Role::findOrFail($id);
+        $role->delete();
+        return response()->json(['message' => 'Role berhasil dihapus.'], 200);
     }
 }
